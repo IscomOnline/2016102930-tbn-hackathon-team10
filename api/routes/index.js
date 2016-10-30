94cf0e2667bf2177510e3228778bb71a597a0a3a
cijie.l <cjlee8110@gmail.com>
2016-10-30 13:21:21 +0800

    Demo v1.0

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,8 +7,52 @@ var pool = mysql.createPool($conf.mysql);
 var router = express.Router();
 
 router.get('/birds', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   pool.getConnection(function (err, connection) {
     connection.query($sql.animals, function (err, result) {
+      if (result) {
+        res.status(200).json(result);
+      }
+      connection.release();
+    });
+  });
+});
+
+router.post('/search', function(req, res, next) {
+  var name = '%' + req.param('name') + '%';
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  pool.getConnection(function (err, connection) {
+    connection.query($sql.search, [name] , function (err, result) {
+      if (result) {
+        res.status(200).json(result);
+      }
+      connection.release();
+    });
+  });
+});
+
+router.post('/detail', function(req, res, next) {
+  var name = req.param('name');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  pool.getConnection(function (err, connection) {
+    connection.query($sql.detail, [name] , function (err, result) {
+      if (result) {
+        res.status(200).json(result);
+      }
+      connection.release();
+    });
+  });
+});
+
+router.post('/chart', function(req, res, next) {
+  var name = req.param('name');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  pool.getConnection(function (err, connection) {
+    connection.query($sql.chart, [name] , function (err, result) {
       if (result) {
         res.status(200).json(result);
       }
